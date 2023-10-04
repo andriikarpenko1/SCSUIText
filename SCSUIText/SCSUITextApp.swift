29104d0d01f2b00c3e8d928f56863a38baec1ed1
Andrii Karpenko <ximerg002@gmail.com>
2023-10-04 17:54:53 +0200

    Initial Commit

---
+++ SCSUIText/SCSUITextApp.swift
@@ -0,0 +1,17 @@
+//
+//  SCSUITextApp.swift
+//  SCSUIText
+//
+//  Created by Andrey Karpenko  on 04.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SCSUITextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
