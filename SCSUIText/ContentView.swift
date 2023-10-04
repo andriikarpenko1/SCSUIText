8bb00ed0dcffe1921f6cd9a83336b3cc2fd3e046
Andrii Karpenko <ximerg002@gmail.com>
2023-10-04 18:54:26 +0200

    Added slider and text

--- SCSUIText/ContentView.swift
+++ SCSUIText/ContentView.swift
@@ -8,12 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var width: Double = 300.0
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+            Group {
+                Text("SwiftUI Marathon ")
+                    .foregroundColor(.black)
+                    .font(.system(size: 16, weight: .regular))
+                + Text("\"Daddy's punch\" ").foregroundColor(.blue).font(.system(size: 20, weight: .bold))
+                + Text("Differrent styling")
+                    .font(.system(size: 18, weight: .semibold))
+                    .foregroundColor(.green)
+            }
+            .multilineTextAlignment(.leading)
+            .frame(width: width)
+            .border(.red)
+
+            Slider(value: $width, in: 100...300)
         }
         .padding()
     }
